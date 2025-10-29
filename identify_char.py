85393d630bcfc7df61638f3c688cb644f24c6ca3
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-10-29 06:32:42 +0530

    Create identify_char.py

---
+++ identify_char.py
@@ -0,0 +1,12 @@
+
+ch = input("Enter a character: ").strip()
+
+
+if len(ch) != 1:
+    print("Error: Please enter exactly one character.")
+else:
+
+    if ch.lower() in 'aeiou':
+        print(f"'{ch}' is a vowel.")
+    else:
+        print(f"'{ch}' is not a vowel.")
